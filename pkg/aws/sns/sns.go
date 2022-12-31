7b24974269c1d4122d14c5a84b7988f3460aee7f
Nam Truong Le <lenamtruong@gmail.com>
2022-12-31 07:55:26 +0100

    feature: add SNS OrderImported (#20)
    Co-authored-by: Nam Truong Le <nam.truong.le@sap.com>

--- pkg/aws/sns/sns.go
+++ pkg/aws/sns/sns.go
@@ -15,11 +15,56 @@ import (
 	"github.com/nam-truong-le/lambda-utils-go/pkg/logger"
 )
 
+const (
+	ssmOrderImportedSNS      = "/sns/orderImported/arn"
+	ssmOrderValidatedSNS     = "/sns/orderValidated/arn"
+	ssmAttachmentRejectedSNS = "/sns/attachmentRejected/arn"
+)
+
+func OrderImported(ctx context.Context, id string) error {
+	log := logger.FromContext(ctx)
+	log.Infof("Send SNS order imported for order [%s]", id)
+
+	snsARN, err := ssm.GetParameter(ctx, ssmOrderImportedSNS, false)
+	if err != nil {
+		return err
+	}
+	snsClient, err := sns.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+	correlationID := ctx.Value(mycontext.FieldCorrelationID)
+	if correlationID == nil {
+		correlationID = uuid.New().String()
+	}
+	message := fmt.Sprintf("Order finalized [%s]", id)
+	_, err = snsClient.Publish(ctx, &awssns.PublishInput{
+		Message: aws.String(message),
+		MessageAttributes: map[string]types.MessageAttributeValue{
+			"id": {
+				DataType:    aws.String("String"),
+				StringValue: aws.String(id),
+			},
+			mycontext.FieldCorrelationID: {
+				DataType:    aws.String("String"),
+				StringValue: aws.String(correlationID.(string)),
+			},
+		},
+		Subject:  aws.String(message),
+		TopicArn: aws.String(snsARN),
+	})
+	if err != nil {
+		log.Errorf("Failed to send SNS order imported [%s]: %s", id, err)
+		return err
+	}
+	return nil
+}
+
 func OrderValidated(ctx context.Context, id string) error {
 	log := logger.FromContext(ctx)
 	log.Infof("Send SNS order validated for order [%s]", id)
 
-	snsARN, err := ssm.GetParameter(ctx, "/sns/orderValidated/arn", false)
+	snsARN, err := ssm.GetParameter(ctx, ssmOrderValidatedSNS, false)
 	if err != nil {
 		return err
 	}
@@ -58,7 +103,7 @@ func AttachmentRejected(ctx context.Context, id string) error {
 	log := logger.FromContext(ctx)
 	log.Infof("Send SNS attachment rejected [%s]", id)
 
-	snsARN, err := ssm.GetParameter(ctx, "/sns/attachmentRejected/arn", false)
+	snsARN, err := ssm.GetParameter(ctx, ssmAttachmentRejectedSNS, false)
 	if err != nil {
 		return err
 	}
