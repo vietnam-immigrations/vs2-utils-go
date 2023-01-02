23f2e2f1b06b5669a8e5ae7b03b8751dd8231367
Nam Truong Le <nam.truong.le@sap.com>
2023-01-02 06:15:21 +0100

    feature: add ssm param const

---
+++ pkg/aws/ssm/params.go
@@ -0,0 +1,13 @@
+package ssm
+
+const (
+	S3BucketAttachment         = "/attachment/s3BucketName"
+	S3BucketInvoice            = "/invoice/s3BucketName"
+	S3BucketResult             = "/result/s3BucketName"
+	SNSTopicAttachmentRejected = "/sns/attachmentRejected/arn"
+	SNSTopicNewOrder           = "/sns/newOrder/arn"
+	SNSTopicNewResult          = "/sns/newResult/arn"
+	SNSTopicOrderImported      = "/sns/orderImported/arn"
+	SNSTopicOrderValidated     = "/sns/orderValidated/arn"
+	SNSTopicResultFound        = "/sns/resultFound/arn"
+)
