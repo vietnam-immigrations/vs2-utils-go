eb52edf1101c5432963a9cef0d2e637c17500853
Nam Truong Le <lenamtruong@gmail.com>
2024-08-08 21:45:21 +0200

    feature: add SendSystemNotification (#489)
    Co-authored-by: Nam Truong Le <nam.truong.le@sap.com>

--- pkg/push/push.go
+++ pkg/push/push.go
@@ -21,3 +21,16 @@ func SendNotificationForOrder(ctx context.Context, orderID, title, message strin
 
 	logs.Add(ctx, db.LogTypeOrder, orderID, fmt.Sprintf("[%s] %s", title, message))
 }
+
+func SendSystemNotification(ctx context.Context, title, message string) {
+	log := logger.FromContext(ctx)
+
+	// send ios push notification
+	err := sendToIOSApp(ctx, title, message)
+	if err != nil {
+		log.Errorf("Failed to send to ios app: %s", err)
+		// will not return here, continue to send pushover
+	}
+
+	logs.Add(ctx, db.LogTypeSystem, "", fmt.Sprintf("[%s] %s", title, message))
+}
